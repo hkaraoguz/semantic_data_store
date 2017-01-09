8a14e83aac0665df5061942ee8d258b4b211eb9d
hkaraoguz <hkaraoguz@yahoo.com.tr>
2017-01-09 17:16:16 +0100

    Code refactoring. Possible removal of mongodb_interface

--- include/util.h
+++ include/util.h
@@ -19,6 +19,7 @@ typedef typename Cloud::Ptr CloudPtr;
 typedef pcl::search::KdTree<PointType> Tree;
 typedef typename SemanticMapSummaryParser::EntityStruct Entities;
 
+// This is the RoomObject struct that contains the ros image, cloud and the deep_net object information
 struct RoomObject
 {
     sensor_msgs::Image rosimage;
@@ -27,6 +28,7 @@ struct RoomObject
 
 };
 
+// This is the Room Observation class that contains the room information vector of ros images and clouds and the room objects
 class RoomObservation
 {
 public:
@@ -38,19 +40,25 @@ public:
     std::vector<RoomObject> roomobjects;
 };
 
-Cloud clampPointCloud(const std::string dimension, Cloud cloud, float maxrange);
+class Util
+{
+public:
 
-RoomObservation readRGBImagesfromRoomSweep(const std::string &observationpath, tf::Vector3 &robotPosition);
+    Cloud clampPointCloud(const std::string dimension, Cloud cloud, float maxrange);
 
-Cloud crop3DObjectFromPointCloud(const deep_object_detection::Object& object, Cloud objectcloud, int image_cols);
+    RoomObservation readRGBImagesfromRoomSweep(const std::string &observationpath, tf::Vector3 &robotPosition);
 
-std::vector< std::pair<deep_object_detection::Object,Cloud> > refineObjects(const std::vector<deep_object_detection::Object> &objects, const std::vector<Cloud> &clouds,
+    Cloud crop3DObjectFromPointCloud(const deep_object_detection::Object& object, Cloud objectcloud, int image_cols);
+
+    std::vector< std::pair<deep_object_detection::Object,Cloud> > refineObjects(const std::vector<deep_object_detection::Object> &objects, const std::vector<Cloud> &clouds,
                                                                             int image_cols, const std::vector<std::string> labels, tf::Vector3 robotPosition);
 
 
-void visualizeDeepNetObjects( std::pair<deep_object_detection::Object,Cloud> apair, sensor_msgs::Image image);
+    void visualizeDeepNetObjects( std::pair<deep_object_detection::Object,Cloud> apair, sensor_msgs::Image image);
 
+    bool logSOMAObjectsToDBCallService(ros::NodeHandle n, SemanticRoom<PointType> aRoom, RoomObservation obs );
 
+};
 
 
 #endif // UTIL_H
